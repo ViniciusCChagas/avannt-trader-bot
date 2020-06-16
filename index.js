8bb58dd9284ac2d368171514c28042cf1482971b
Vinicius Chagas <vinicius_decastro@hotmail.com>
2020-06-15 22:07:07 -0300

    Adicionado envio do calendário econômico pelo investing.com

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ app.get("/sendPanoram", async (request, response) => {
   return response.json({ panorama });
 });
 
+app.get("/sendEconomicCalendar", async (request, response) => {
+  const economicCalendar = await panoram.sendEconomicCalendar();
+  return response.json({ economicCalendar });
+});
+
 //port for Avannt Trader Bot = 3334
 app.listen(3334);
 
