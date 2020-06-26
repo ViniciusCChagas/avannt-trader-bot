5d9bd63dbc461cb6442b5ca662fb5881f774ad02
Vinicius Chagas <vinicius_decastro@hotmail.com>
2020-06-25 21:31:45 -0300

    Added players position

--- routes.js
+++ routes.js
@@ -2,11 +2,22 @@ const express = require("express");
 
 const PanoramController = require("./src/controllers/PanoramController");
 const EconomicCalendarController = require("./src/controllers/EconomicCalendarController");
+const PlayersPositionController = require("./src/controllers/PlayersPositionController");
 
 const routes = express.Router();
 
 routes.get("/sendPanoram", PanoramController.sendPanoram);
 
 routes.get("/sendEC", EconomicCalendarController.sendEconomicCalendar);
+
+routes.get("/sendPP", PlayersPositionController.sendPlayersPosition);
+
+routes.get("/sendAll", async (req, res)=>{
+    let text = '';
+    text += await PanoramController.sendPanoram();
+    text += await PlayersPositionController.sendPlayersPosition();
+    text += await EconomicCalendarController.sendEconomicCalendar();
+    return res.json({ text });
+});
 
 module.exports = routes;
--- src/services/CronJobService.js
+++ src/services/CronJobService.js
@@ -1,6 +1,7 @@
 const cron = require("node-cron");
 const PanoramController = require("../controllers/PanoramController");
 const EconomicCalendarController = require("../controllers/EconomicCalendarController");
+const PlayersPositionController = require("../controllers/PlayersPositionController");
 
 /**
  * @description inits the cronJobService
@@ -9,6 +10,7 @@ function init() {
   //Cron Schedule for senPanoram  and sendEC (Every weekday at 8:30am)
   const cronPanoram = cron.schedule("30 8 * * 1-5", async () => {
     await PanoramController.sendPanoram();
+    await PlayersPositionController.sendPlayersPosition();
     await EconomicCalendarController.sendEconomicCalendar();
   });
   cronPanoram.start();
